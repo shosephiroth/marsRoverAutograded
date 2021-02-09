0f7f6d9a53683e6b27074b0122f81fd6c6b77586
Sally Steuterman <sally.steuterman@gmail.com>
2021-02-08 18:25:47 -0600

    Updated starter code

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -3,6 +3,19 @@ const Command = require('../command.js');
 
 describe("Message class", function() {
 
-  // 3 tests here!
+  it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+    expect( function() { new Message();}).toThrow(new Error('Message name required.'));
+  });
+
+  it("constructor sets name", function() {
+    let message = new Message('New message!');
+    expect(message.name).toEqual('New message!');
+  });
+
+  it("contains a commands array passed into the constructor as 2nd argument", function() {
+    let commands = [new Command('STATUS_CHECK'), new Command('MOVE', 20)];
+    let message = new Message('Another message!', commands);
+    expect(message.commands).toEqual(commands);
+  });
 
 });
