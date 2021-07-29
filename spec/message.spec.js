e7d5c4d44810b1d75b170d251fb8c11f5265d794
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-28 23:02:52 -0400

    Update message.spec.js
    I think I correctly wrote my first test? I also am not sure if the very first part of the Rover assignment, was just tests that they already had set. I also found a typo I believe.

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -4,6 +4,14 @@ const Command = require('../command.js');
 // NOTE: If at any time, you want to focus on the output from a single test, feel free to comment out all the others.
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
-describe("Message class", function() {
+describe("Message class", function () {
+  it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+    expect( function() { new Message();}).toThrow(new Error('Name required'))
+  })
 
 });
+
+  // Use this as a guide from command.spec.js:
+  // it("throws error if command type is NOT passed into constructor as the first parameter", function() {
+  //   expect( function() { new Command();}).toThrow(new Error('Command type required.'));
+  // });
