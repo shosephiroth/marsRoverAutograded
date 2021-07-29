830dc110039d3634a3e3f6dd24a5161f5799e1f6
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-29 10:34:43 -0400

    Update message.js
    If I did this correctly, I wrote the correct code for the test I wrote last night. Wanted to save state here first in case replit acts up.

--- message.js
+++ message.js
@@ -1,5 +1,26 @@
 class Message {
    // Write code here!
+   constructor(messageType, value) {
+     this.messageType = messageType;
+     if (!messageType) {
+       throw Error("Name required.");
+     }
+     this.value = value;
+   }
+   
 }
+
+// use this as a reference, from command.js:
+
+// class Command {
+//    constructor(commandType, value) {
+//      this.commandType = commandType;
+//      if (!commandType) {
+//        throw Error("Command type required.");
+//      }
+//      this.value = value;
+//    }
+ 
+//  }
 
 module.exports = Message;
