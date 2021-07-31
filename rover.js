10a0bffcccf05415833767e6778f3daa7e2d2f3c
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-07-31 18:53:42 -0400

    Update rover.js
    I may be doing this too soon but I got code running again finally

--- rover.js
+++ rover.js
@@ -1,5 +1,15 @@
 class Rover {
    // Write code here!
+   constructor(position, mode, generatorWatts) {
+     this.position = position;
+     this.mode = 'NORMAL';
+     this.generatorWatts = 110;
+
+   }
+
+  receiveMessage(message) {
+
+  }
 }
 
 module.exports = Rover;
