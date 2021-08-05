7b54e9a86e250ae1bb04ad02bb63bd5cfdd01dfe
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-08-05 03:08:23 +0000

    Ok real final submit this time

--- rover.js
+++ rover.js
@@ -1,10 +1,36 @@
 class Rover {
+  // Write code here!
+   // define constructor with 1 param (position)
+      // set position
+      // set mode 
+      // set generatorWatts'
 	constructor(position) {
 		this.position = position;
 		this.mode = 'NORMAL';
 		this.generatorWatts = 110;
 	}
 
+  //define a function called receiveMessage(message)
+    //return {
+        // message: message name
+        // results: this.buildResults(message)
+    // } 
+      //buildResults(message){
+    // initialize result array
+    // go through each command
+    // check what type of command it is
+    // if command is status check
+        // add this to the result array
+      // {
+      //   completed: true, 
+      //   roverStatus: {
+      //     mode: 'NORMAL', 
+      //     generatorWatts: 110,
+      //     position: 87382098
+      //   }
+      // }
+  //}
+    
 	receiveMessage(message) {
 		let response = {
 			message: message.name,
